fe2f8a032fbed7b9b8846ffb395249b8c0c551c3
alfiovavassori <vavasa@usi.ch>
2022-11-30 23:04:15 +0100

    added

--- IR_project/spiders/footballscraper.py
+++ IR_project/spiders/footballscraper.py
@@ -1,11 +1,18 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 
-class BookScraper(CrawlSpider):
-    name = "footballscraper"
-    start_urls = ["https://footballdatabase.com/ranking/world/1"]
+class BookScraper(scrapy.Spider):
+    name = 'footballscraper'
+    start_urls = ['https://footballdatabase.com/ranking/world/1']
 
 
     def parse(self, response):
-        return 0
+        for teams in response.css('tbody tr'):
+
+            try:
+                yield {
+                    'rank': teams.css('td:nth-child(1)::text').get(),
+                    'team_name': teams.css('.limittext::text').get(),
+                    'points': teams.css('td:nth-child(3)::text').get(),
+                }
+            except:
+                print("Error")
--- IR_project/spiders/footballscraper_fifa.py
+++ IR_project/spiders/footballscraper_fifa.py
@@ -2,9 +2,20 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-class BookScraper(CrawlSpider):
-    name = "bookscraper"
-    start_urls = ["https://www.fifa.com/fifa-world-ranking/men?dateId=id13792"]
+class BookScraper(scrapy.Spider):
+    name = 'footballrankings'
+    start_urls = ['https://football-ranking.com/fifa_rankings']
+
 
     def parse(self, response):
-        return 0
+        for teams in response.css('.ml-1 > tbody tr'):
+
+            try:
+                print('here')
+                yield {
+                    'rank': teams.css('td::text').get()[0:2],
+                    'team_name': teams.css('img').get()[15:18],
+                    'points': teams.css('b::text').get(),
+                }
+            except:
+                print("Error")

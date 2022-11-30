760ec6b3d856f48c0cb9ce007545bcc214d77fe5
Alessandro Cravioglio <41857473+alecrav@users.noreply.github.com>
2022-11-30 17:03:39 +0100

    Add files via upload

---
+++ footballscraper.py
@@ -0,0 +1,19 @@
+
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class BookScraper(CrawlSpider):
+    name = 'footballscraper'
+    start_urls = ['https://footballdatabase.com/ranking/world/1']
+
+
+    def parse(self, response):
+        for teams in response.css('tbody tr'):
+
+            yield {
+                'rank': teams.css('td.nth-child(1)::text').get(),
+                'team_name': teams.css('.limittext::text').get(),
+                'points': teams.css('td:nth-child(3)::text').get(),
+            }

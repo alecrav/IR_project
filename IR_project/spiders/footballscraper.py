21cd9026fa5238e502c92fb3089d84731fa5268d
alfiovavassori <vavasa@usi.ch>
2022-11-30 16:57:16 +0100

    added

--- IR_project/spiders/footballscraper.py
+++ IR_project/spiders/footballscraper.py
@@ -3,9 +3,15 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 class BookScraper(CrawlSpider):
-    name = "footballscraper"
-    start_urls = ["https://footballdatabase.com/ranking/world/1"]
+    name = 'footballscraper'
+    start_urls = ['https://footballdatabase.com/ranking/world/1']
 
 
-    def parse_book(self, response):
-        return 0
+    def parse(self, response):
+        for teams in response.css('tbody tr'):
+
+            yield {
+                'rank': teams.css('td.nth-child(1)::text').get(),
+                'team_name': teams.css('.limittext::text').get(),
+                'points': teams.css('td:nth-child(3)::text').get(),
+            }

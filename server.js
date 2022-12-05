b277984d0ea7cc996a80d44fe4b810f9c9916467
alecrav <alessandrocrav@bluewin.ch>
2022-12-05 11:35:00 +0100

    change server

--- server.js
+++ server.js
@@ -10,40 +10,6 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
-function req_function(first, second, num) {
-    let query = 'http://localhost:8983/solr/football/query?q=' + first + '%3A' + second + '&q.op=OR&indent=true&rows=' + num;
-    
-    let req = https.get(query, (res) => {
-        if (res.statusCode !== 200) {
-          console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-          res.resume();
-          return;
-        }
-    
-        let data = '';
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-        
-        res.on('end', () => {
-            console.log('Retrieved all data');
-            try {
-                console.log(data);
-                return data;
-            } catch(e) {
-                console.error(e);
-            }
-        });
-
-    });
-    
-    req.on('error', (err) => {
-        console.error(`Encountered an error trying to make a request: ${err.message}`);
-    });
-    req.end();
-
-}
-
 // endpoints
 
 app.get('/', function(req, res) {
@@ -54,10 +20,9 @@ app.get('/query/:field/:value/:num', function (req, res) {
     let _field = req.params.field;
     let _value = req.params.value;
     let _num = req.params.num;
-
-    // if (req.accepts('application/json')) {
-        // res.status(200).type('application/json').json(req_function(_field, _value, _num));
-    // }
+    if (isNan(_num)) {
+        _num = 10;
+    }
 
     let url = 'http://localhost:8983/solr/football/query?q=' + _field + '%3A' + _value + '&q.op=OR&indent=true&rows=' + _num;
     
@@ -71,7 +36,7 @@ app.get('/query/:field/:value/:num', function (req, res) {
         }
     })
     .then(function (response) {
-        res.send(JSON.stringify(response.data));
+        res.status(200).send(JSON.stringify(response.data));
     })
     .catch(function (error) {
         console.log(error);
